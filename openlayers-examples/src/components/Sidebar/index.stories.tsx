fa1e78b12d0bc68e3f63a84193ef7f090459c672
hyunjin <leehj0110@kakao.com>
2023-02-25 15:31:26 +0900

    chore: storybook 제거

---
+++ openlayers-examples/src/components/Sidebar/index.stories.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Sidebar from '.';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'components/Sidebar',
+  component: Sidebar,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Sidebar>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Sidebar> = (args) => (
+  <Sidebar {...args} />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+// Primary.args = {
+//   primary: true,
+//   label: 'Button',
+// };
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
---
+++ openlayers-examples/src/components/Sidebar/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Sidebar: React.FC = () => {
+  return (
+    <Wrapper>
+      <SidebarIcon />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div``;
+
+const IconImage = styled.img`
+  width: 17px;
+  height: 15.94px;
+  margin-top: calc(22px - 8px);
+`;
+
+const SidebarIcon = () => {
+  return <Link to="/temp"></Link>;
+};
+
+export default Sidebar;
